6ee519c49c656293953521b33d7b0bfce11fb0eb
WaqarHameed1113 <45306874+WaqarHameed1113@users.noreply.github.com>
2018-12-07 17:45:47 +0500

    Adding Execution time in Matlab & Sequenctial Code
    Adding Execution time on both matlab and sequential program and start implementation on parallel algorithm

---
+++ Code Implementations/Cuda Implementation/Parallel/project/kernel.cu
@@ -0,0 +1,151 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+void training();
+void CosinKernel(float x, float y, int CenterR, int CenterC, float Centers[][121],float* output);
+void GaussianKernal(float x, float y, int CenterR, int CenterC, float Centers[][121], float* output);
+int main()
+{
+	const int N = 121;
+	size_t size = N * sizeof(float);
+	
+	float train[121][2];
+	float trainOutput[121];
+	float Centers[2][121] = { { -1,-0.8,-0.6,-0.4,-0.2,0,0.2,0.4,0.6,0.8,1,-1,-0.8,-0.6,-0.4,-0.2,0,0.2,0.4,0.6,0.8,1,-1,-0.8,-0.6,-0.4,-0.2,0,0.2,0.4,0.6,0.8,1,-1,-0.8,-0.600000000000000,-0.400000000000000,-0.200000000000000,0,0.200000000000000,0.400000000000000,0.600000000000000,0.800000000000000,1,-1,-0.800000000000000,-0.600000000000000,-0.400000000000000,-0.200000000000000,0,0.200000000000000,0.400000000000000,0.600000000000000,0.800000000000000,1,-1,-0.800000000000000,-0.600000000000000,-0.400000000000000,-0.200000000000000,0,0.200000000000000,0.400000000000000,0.600000000000000,0.800000000000000,1,-1,-0.800000000000000,-0.600000000000000,-0.400000000000000,-0.200000000000000,0,0.200000000000000,0.400000000000000,0.600000000000000,0.800000000000000,1,-1,-0.800000000000000,-0.600000000000000,-0.400000000000000,-0.200000000000000,0,0.200000000000000,0.400000000000000,0.600000000000000,0.800000000000000,1,-1,-0.800000000000000,-0.600000000000000,-0.400000000000000,-0.200000000000000,0,0.200000000000000,0.400000000000000,0.600000000000000,0.800000000000000,1,-1,-0.800000000000000,-0.600000000000000,-0.400000000000000,-0.200000000000000,0,0.200000000000000,0.400000000000000,0.600000000000000,0.800000000000000,1,-1,-0.800000000000000,-0.600000000000000,-0.400000000000000,-0.200000000000000,0,0.200000000000000,0.400000000000000,0.600000000000000,0.800000000000000,1 },
+							  { -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-0.8,-0.8,-0.8,-0.8,-0.8,-0.8,-0.8,-0.8,-0.8,-0.8,-0.8,-0.6,-0.6,-0.6,-0.6,-0.6,-0.6,-0.6,-0.6,-0.6,-0.6,-0.6,-0.4,-0.4,-0.4,-0.400000000000000,-0.400000000000000,-0.400000000000000,-0.400000000000000,-0.400000000000000,-0.400000000000000,-0.400000000000000,-0.400000000000000,-0.200000000000000,-0.200000000000000,-0.200000000000000,-0.200000000000000,-0.200000000000000,-0.200000000000000,-0.200000000000000,-0.200000000000000,-0.200000000000000,-0.200000000000000,-0.200000000000000,0,0,0,0,0,0,0,0,0,0,0,0.200000000000000,0.200000000000000,0.200000000000000,0.200000000000000,0.200000000000000,0.200000000000000,0.200000000000000,0.200000000000000,0.200000000000000,0.200000000000000,0.200000000000000,0.400000000000000,0.400000000000000,0.400000000000000,0.400000000000000,0.400000000000000,0.400000000000000,0.400000000000000,0.400000000000000,0.400000000000000,0.400000000000000,0.400000000000000,0.600000000000000,0.600000000000000,0.600000000000000,0.600000000000000,0.600000000000000,0.600000000000000,0.600000000000000,0.600000000000000,0.600000000000000,0.600000000000000,0.600000000000000,0.800000000000000,0.800000000000000,0.800000000000000,0.800000000000000,0.800000000000000,0.800000000000000,0.800000000000000,0.800000000000000,0.800000000000000,0.800000000000000,0.800000000000000,1,1,1,1,1,1,1,1,1,1,1 } };
+	float data[11];
+	data[0] = -1.0;
+	float alpha1 = 0.5;
+	float alpha2 = 0.5;
+	float outputNeuron = 1;
+	double w[121];
+	double b = 0.0;
+	double error;
+	double learningRate = 0.001;
+	int Epoch = 10000;
+	for (int i = 0; i < 121; i++)
+	{
+		w[i] = 0.0;
+	}
+	for (int i = 1; i < 11; i++)
+	{
+		data[i] = data[i - 1] + 0.2;
+		//printf("%f\n", data[i]);
+	}
+	int count = 0;
+	for (int i = 0; i < 11; i++)
+	{
+		for (int j = 0; j < 11; j++)
+		{
+			train[count][0] = data[j];
+			train[count][1] = data[i];
+			trainOutput[count] = exp(-pow(train[count][0], 2) - train[count][1]);
+			//printf("%.2f, %.2f %.2f\n", train[count][0], train[count][1], trainOutput[count]);
+			count++;
+		}
+	}
+	
+	float *KC,*KG;
+
+	KC = (float *)malloc(size);
+	KG = (float *)malloc(size);
+	double RBFoutput[121];
+	double finalOutput = 0.0;
+	clock_t tStart = clock();
+	for (int Epoch = 0; Epoch < 10000; Epoch++)
+	{
+		for (int sample = 0; sample < 121; sample++)
+		{
+			CosinKernel(train[sample][0], train[sample][1], 2, 121, Centers, KC);
+			GaussianKernal(train[sample][0], train[sample][1], 2, 121, Centers, KG);
+			finalOutput = 0.0;
+			for (int i = 0; i < 121; i++)
+			{
+				RBFoutput[i] = (alpha1*KC[i] + alpha2*KG[i]) / (alpha1 + alpha2);
+				finalOutput += RBFoutput[i] * w[i] ;
+
+			}
+			finalOutput = finalOutput + b;
+		//	printf("Final: %f\n", finalOutput);
+			error = trainOutput[sample] - finalOutput;
+		//	printf("Error: %f\n", error);
+			b = b + learningRate*error;
+		//	printf("b: %f\n\n", b);
+			for (int i = 0; i < 121; i++)
+			{
+				w[i] = w[i] + learningRate*error*RBFoutput[i];
+				//printf("W: %f\n", w[i]);
+			}
+			double alpha1Update = 0.0, alpha2Update = 0.0;
+			for (int i = 0; i < 121; i++)
+			{
+				alpha1Update += (w[i] * (KC[i] - KG[i]));//l*((alpha1*alpha2) / (alpha1*pow(alpha1 + alpha2, 2)));
+
+			}
+			//printf("Alpha1UP: %f\n", alpha1Update);
+			for (int i = 0; i < 121; i++)
+			{
+				alpha2Update += (w[i] * (KG[i] - KC[i]));//l*((alpha1*alpha2) / (alpha1*pow(alpha1 + alpha2, 2)));
+
+			}
+			//printf("Alpha2UP: %f\n", alpha2Update);
+			alpha1Update = error*alpha1Update*((alpha1*alpha2) / (alpha1*pow(alpha1 + alpha2, 2)));
+
+
+
+			alpha2Update = error*alpha2Update*((alpha1*alpha2) / (alpha2*pow(alpha1 + alpha2, 2)));
+			alpha1 = alpha1 + learningRate*alpha1Update;
+			alpha2 = alpha2 + learningRate*alpha2Update;
+			alpha1 =  alpha1 / (alpha1 + alpha2);
+			alpha2 = alpha2 / (alpha1 + alpha2);
+			
+		}
+		
+	}
+	printf("Alpha1: %f\n", alpha1);
+	printf("Alpha2: %f\n", alpha2);
+	printf("Time taken: %.2fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+	return 0;
+}
+
+
+
+void GaussianKernal(float x, float y,  int CenterR, int CenterC, float Centers[][121],float* output)
+{
+	
+	float sigma = 0.04;
+//	printf("Gauss Kernel\n\n\n");
+	for (int i = 0; i < 121; i++)
+	{
+		output[i] = exp(-(pow((x - Centers[0][i]), 2) + pow((y - Centers[1][i]), 2))/0.04);
+	//	printf("%f\n", output[i]);
+	}
+
+}
+void CosinKernel(float x,float y, int CenterR, int CenterC,float Centers[][121] , float* output)
+{
+//	printf("Cosine Kernel\n");
+//	
+
+	//float output[121];
+	float sumCenter[121];
+	float intputsq=x*x +y*y;
+//	printf("\nMultiplication Kernel\n\n\n");
+	
+	for (int i = 0; i < 121; i++)
+	{
+		float sum = 0.0;
+		sum = x * Centers[0][i]+ y * Centers[1][i];
+		output[i] = sum;
+		
+		sumCenter[i] = sqrt((pow(Centers[0][i], 2) + pow(Centers[1][i], 2))*intputsq);
+		output[i] = output[i] / (sumCenter[i]+0.0000000000000001);
+		//printf("%f\n", output[i]);
+	}
+	
+}
--- Code Implementations/Matlab Implementation/Main.m
+++ Code Implementations/Matlab Implementation/Main.m
@@ -34,14 +34,6 @@ alpha2=0.5;%initilze alpha2
 outputNeuron=1; % Number of output Neuron
 [w_fused,b_fused]=InitilizeNetwork(TrainInput,alpha1,alpha2,outputNeuron,Centers);  % initialize network with weight and bias vectors
 
-w_cosine=w_fused;   % Initilize cosine Weight
-b_cosine=b_fused;   % Initilize cosine Bias
-
-w_gauss=w_fused;    % Initilize Guass Weight
-b_gauss=b_fused;    % Initilize Gause Bias
-
-w_manual=w_fused;   % Initilize Manual Weight
-b_manual=b_fused;   % Initilize Manual Bias
 
 learningRate=1e-3; % Initilize Learning Rate
 epoch=10000;     % number of Epoch
@@ -49,7 +41,7 @@ preAlpha1=alpha1; % strore previous Value of alpha1
 preAlpha2=alpha2;% strore previous Value of alpha2
 e=zeros(4,size(TrainInput,1));
 et=zeros(epoch,4);
-
+tic
 for z=1:epoch
     for i=1:(size(TrainInput,1))
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%% Fussesd Kernel %%%%%%%%%%%%%%
@@ -64,48 +56,23 @@ for z=1:epoch
         preAlpha1=alpha1;% hold previous value of alpha1
         preAlpha2=alpha2;% hold previouds value of alpha 2
         
-        %%%%%%%%%%%%%%%%%%%%%% Cosine Kernel %%%%%%%%%%%%%%%%%%%%%%%
-        
-        %For Cosine Kernel Alpha1=1 and Alpha2=0, put in below equation
-        [error,Y1,Y2,KC,KG]=Forward(TrainInput(i,:),1,0,w_cosine,b_cosine,Centers,TrainOutput(i,:)');  % Forward Path
-        e(2,i)=sum(error.^2); % Accumulate Error of Each Training Set
-        w_cosine=w_cosine+(learningRate)*error.*Y1;   %Update Weight
-        b_cosine=b_cosine+(learningRate)*error;       %Update Bias
-        
-        %%%%%%%%%%%%%%%%%%%%%% Gauss Kernel %%%%%%%%%%%%%%%%%%%%%%%%
-        %For Gauss Kernel Alpha1=0 and Alpha2=1, put in below equation
-        [error,Y1,Y2,KC,KG]=Forward(TrainInput(i,:),0,1,w_gauss,b_gauss,Centers,TrainOutput(i,:)');  % ForwardPath to netword
-        e(3,i)=sum(error.^2); % Accumulate Error of Each Training Set
-        w_gauss=w_gauss+(learningRate)*error.*Y1;   %Update Weight
-        b_gauss=b_gauss+(learningRate)*error;       %Update Bias
-        
-        %%%%%%%%%%%%%%%%%%%% Manualy Fussed %%%%%%%%%%%%%%%%%%%
-        % for Manual we put alpha1=0.5 and alpha2=0.5
-        [error,Y1,Y2,KC,KG]=Forward(TrainInput(i,:),0.5,0.5,w_manual,b_manual,Centers,TrainOutput(i,:)');  % ForwardPath to netword
-        e(4,i)=sum(error.^2); % Accumulate Error of Each Training Set
-        w_manual=w_manual+(learningRate)*error.*Y1;   %Update Weight
-        b_manual=b_manual+(learningRate)*error;       %Update Bias
+       
     end
     
     et(z,1)=db(sum(e(1,:)),'power'); % finding MSE of Fused Kernel on Each Epoch in dB
-    et(z,2)=db(sum(e(2,:)),'power'); % finding MSE of Cosine Kernel on Each Epoch in dB
-    et(z,3)=db(sum(e(3,:)),'power'); % finding MSE of Gauss Kernel on Each Epoch in dB
-    et(z,4)=db(sum(e(4,:)),'power'); % finding MSE of Manual Kernel on Each Epoch in dB
-    %et(z)=mse(e); % finding MSE on Each Epoch in dB
+   
     ap1(z)=alpha1;            % alpha1 value on Each Epoch
     ap2(z)=alpha2;            % alpha2 value on Each Epoch
 end
+toc
 %%%%%%%%%%%%%%%%%%%%%%%% PLOTING AREA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure(1)
 plot(et(:,1),'Color','green','LineWidth',2);
-hold on;
-plot(et(:,2),'--','Color','red','LineWidth',2);
-plot(et(:,3),'--','Color','blue','LineWidth',2);
-plot(et(:,4),'-','Color','black','LineWidth',2);
+
 grid on;
 xlabel('Epoch');
 ylabel('RMS(db)');
-legend('Dynamic Fussed Kernel','Cosine Kernel', 'Gauss Kernel','Manual');
+legend('Dynamic Fussed Kernel');
 
 figure(2)
 plot(ap1,'--','LineWidth',2,'Color','red');
@@ -124,26 +91,15 @@ for k=1:size(TestInput,1)
     [error,Y1,Y2,KC,KG]=Forward(TestInput(k,:),alpha1,alpha2,w_fused,b_fused,Centers,TestOutput(k,:)'); %Send Input to Network To Get Output
     acumulator(1,k)=Y2;
     
-    [error,Y1,Y2,KC,KG]=Forward(TestInput(k,:),1,0,w_cosine,b_cosine,Centers,TestOutput(k,:)'); %Send Input to Network To Get Output
-    acumulator(2,k)=Y2;
-    
-    [error,Y1,Y2,KC,KG]=Forward(TestInput(k,:),0,1,w_gauss,b_gauss,Centers,TestOutput(k,:)'); %Send Input to Network To Get Output
-    acumulator(3,k)=Y2;
-    
-    [error,Y1,Y2,KC,KG]=Forward(TestInput(k,:),0.5,.5,w_manual,b_manual,Centers,TestOutput(k,:)'); %Send Input to Network To Get Output
-    acumulator(4,k)=Y2;
+   
     
 end
 
 figure(3)
-plot(acumulator(3,:),'--','LineWidth',2,'Color','blue');
-hold on;
-plot(acumulator(2,:),'--','LineWidth',2,'Color','red');
-plot(acumulator(4,:),'-.','LineWidth',2,'Color','black');
-plot(TestOutput','LineWidth',2,'Color','Yellow');
+
 plot(acumulator(1,:),'LineWidth',2,'Color','green');
 grid on;
-legend('Gauss Kernel based RBF','Cosine Kernel based RRBF','Manual fusion of Gauss & Cosine Kernels', 'Actual Output','Proposed dynamic Fusion of Gauss & Cosine Kernels');
+legend('Proposed dynamic Fusion of Gauss & Cosine Kernels');
 hold off
 alpha1
 alpha2
